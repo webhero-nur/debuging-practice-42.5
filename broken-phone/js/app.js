7d27384f552efac3a462887d7f5d41508f48fd37
webhero-nur <nurealom522@gmail.com>
2023-01-26 11:43:14 +0600

    broken-phone: initial phone loading fixed

--- broken-phone/js/app.js
+++ broken-phone/js/app.js
@@ -15,7 +15,7 @@ const displayPhones = (phones, dataLimit) => {
         showAll.classList.remove('d-none');
     }
     else {
-        showAll.classList.add('d-hidden');
+        showAll.classList.add('d-none');
     }
 
 
@@ -31,9 +31,10 @@ const displayPhones = (phones, dataLimit) => {
     phones.forEach(phone => {
         const phoneDiv = document.createElement('div');
         phoneDiv.classList.add('col');
-        phonesContainer.innerHTML = `
+        console.log(phone);
+        phoneDiv.innerHTML = `
         <div class="card p-4">
-            <img src="${phone.images}" class="card-img-top" alt="...">
+            <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -105,4 +106,4 @@ const displayPhoneDetails = phone => {
     `
 }
 
-loadPhones('apple');
+loadPhones('a', true);
